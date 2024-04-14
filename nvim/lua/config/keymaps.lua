e612fad48ae4b3362615477a06bd9ff6467b7fe6
Neelzee <nilsien2001@gmail.com>
2024-04-14 11:03:19 +0200

    removed arrow-keys

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Delete arrow key mappings
+vim.keymap.set({ "n", "i", "v" }, "<Up>", function() end)
+vim.keymap.set({ "n", "i", "v" }, "<Down>", function() end)
+vim.keymap.set({ "n", "i", "v" }, "<Left>", function() end)
+vim.keymap.set({ "n", "i", "v" }, "<Right>", function() end)
