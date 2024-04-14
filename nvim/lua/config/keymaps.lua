ab2d348053316f6aa83f11e9d8ae0ac5befe8460
Neelzee <nilsien2001@gmail.com>
2024-04-14 11:13:24 +0200

    fixed clipboard

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
 -- Delete arrow key mappings
 vim.keymap.set({ "n", "i", "v" }, "<Up>", function() end)
 vim.keymap.set({ "n", "i", "v" }, "<Down>", function() end)
