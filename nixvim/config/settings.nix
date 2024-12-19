69d0b8d86bc1508ab165d377140c63c2d1af8f40
Neelzee <nilsien2001@gmail.com>
2024-12-19 16:02:08 +0100

    feat: added linenumbers

--- nixvim/config/settings.nix
+++ nixvim/config/settings.nix
@@ -1,4 +1,10 @@
 { config, pkgs, inputs, ... }:
 {
     clipboard.register = "unnamedplus";
+    opts = {
+      number = true;         # Show line numbers
+      relativenumber = true; # Show relative line numbers
+
+      shiftwidth = 2;        # Tab width should be 2
+    };
 }
