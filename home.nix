fe6367c88573140ff8a06c8a602305532cf70a9c
Neelzee <nilsien2001@gmail.com>
2024-12-19 16:31:50 +0100

    feat: removed shell init. from nmide

--- home.nix
+++ home.nix
@@ -15,7 +15,7 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      nmide = "cd ~/Documents/nmide && nix-shell";
+      nmide = "cd ~/Documents/nmide";
       config = "cd ~/Documents/nix-config";
       rebuildOs = "sudo nixos-rebuild switch --flake ~/Documents/nix-config#laptop";
       rebuildHa = "home-manager switch -f ~/Documents/nix-config/hosts/laptop/home.nix";
