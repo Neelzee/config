edcce4c5deef05be695671b79c9e5419726f890b
Neelzee <nilsien2001@gmail.com>
2024-12-19 16:11:02 +0100

    fix(home): corrected config/nmide alias

--- home.nix
+++ home.nix
@@ -14,8 +14,8 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      nmide = "cd ~/Documents/nmide";
-      config = "cd ~/Documents/nix-config && nix-shell";
+      nmide = "cd ~/Documents/nmide && nix-shell";
+      config = "cd ~/Documents/nix-config";
       rebuildOs = "sudo nixos-rebuild switch --flake ~/Documents/nix-config#laptop";
       rebuildHa = "home-manager switch -f ~/Documents/nix-config/hosts/laptop/home.nix";
     };
