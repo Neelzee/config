11f02e9dba00eac6f7d54b088ad39e3008312951
Neelzee <nilsien2001@gmail.com>
2024-12-27 13:34:20 +0100

    refactor: refactored home.nix & configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,5 @@
 {
-  config,
   pkgs,
-  inputs,
   ...
 }:
 
@@ -26,17 +24,19 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "no";
-    variant = "";
+  services = {
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+      # Enable the GNOME Desktop Environment.
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      # Configure keymap in X11
+      xkb = {
+        layout = "no";
+        variant = "";
+      };
+    };
   };
 
   programs.steam = {
@@ -71,7 +71,7 @@
       "networkmanager"
       "wheel"
     ];
-    packages = with pkgs; [
+    packages = [
       #  thunderbird
     ];
   };
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ ... }:
 {
   imports = [
     ./modules/programs/alacritty.nix
@@ -8,24 +8,25 @@
     ./modules/languages.nix
   ];
 
-  home.username = "nmf";
-  home.homeDirectory = "/home/nmf";
-
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      nmide = "cd ~/Documents/nmide";
-      config = "cd ~/Documents/nix-config";
-      rebuildOs = "sudo nixos-rebuild switch --flake ~/Documents/nix-config#laptop";
-      rebuildHa = "home-manager switch -f ~/Documents/nix-config/hosts/laptop/home.nix";
+  home = {
+    username = "nmf";
+    homeDirectory = "/home/nmf";
+    stateVersion = "24.11"; # Please read the comment before changing.
+    packages = [ ];
+    sessionVariables = {
+      EDITOR = "nvim";
     };
   };
 
-  home.packages = with pkgs; [ ];
-
-  home.sessionVariables = {
-    EDITOR = "nvim";
+  programs = {
+    bash = {
+      enable = true;
+      shellAliases = {
+        nmide = "cd ~/Documents/nmide";
+        config = "cd ~/Documents/nix-config";
+        rebuildOs = "sudo nixos-rebuild switch --flake ~/Documents/nix-config#laptop";
+        rebuildHa = "home-manager switch -f ~/Documents/nix-config/hosts/laptop/home.nix";
+      };
+    };
   };
 }
